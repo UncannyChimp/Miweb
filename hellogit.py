c75887e64a72130b7c860e37f49610a3aa7bacb8
UncannyChimp <jose-san@live.com>
2023-01-21 12:22:56 -0600

    Pruebas de Python

---
+++ hellogit.py
@@ -0,0 +1,21 @@
+#Primera prueba de cambios
+var = 0
+
+fin = 0
+
+veces = input("Cuantas veces quieres mostrar: ")
+
+try: 
+    veces = int(veces)
+except:
+    veces = "Equivocado"
+
+    if veces=="Equivocado":
+        print("El valor esta equivocado")
+        exit()
+
+while fin < veces:
+    var = var + 1
+    fin = var
+    print(fin)
+    
---
+++ hellogit2.py
@@ -0,0 +1,30 @@
+#Primera prueba de cambios
+comp = 10
+
+var = 0
+
+fin = 0
+
+otro = 0
+
+veces = input("Cuantas veces quieres mostrar: ")
+
+try: 
+    veces = int(veces)
+except:
+    veces = "Equivocado"
+
+    if veces=="Equivocado":
+        print("El valor esta equivocado")
+        exit()
+
+while fin < veces:
+    var = var + 1
+
+    while comp > 1:
+    otro = var / 3
+    print comp
+    exit()
+
+    fin = var
+    print(fin)
